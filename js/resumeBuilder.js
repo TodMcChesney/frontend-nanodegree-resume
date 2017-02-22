17d1205b3b69b58d5561f06f9fb4d3ebf7fe7a98
TodMcChesney <TodMcChesney@users.noreply.github.com>
2017-02-22 11:14:36 +0100

    Add all 4 objects (with data) for project

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,43 +1,98 @@
+var work = {
+    jobs: [
+        {
+            employer: 'Self Employed',
+            title: 'Front-End Web Developer',
+            location: 'Geneva, Switzerland',
+            dates: 'Oct 2009 - Present',
+            description: 'Front-end web development and web consulting'
+        },
+        {
+            employer: 'Alabama Outdoors',
+            title: 'eCommerce Manager',
+            location: 'Birmingham, Alabama',
+            dates: 'Apr 2013 - Apr 2016',
+            description: 'Managed all eCommerce operations'
+        },
+        {
+            employer: "Mountain High Outfitters",
+            title: 'eCommerce Manager',
+            location: 'Birmingham, Alabama',
+            dates: 'Oct 2011 - Apr 2013',
+            description: 'Managed all eCommerce operations'
+        }
+    ]
+};
+
+var projects = {
+    projects: [
+        {
+            title: 'AlabamaCupRaces.com Website',
+            dates: 'Jan 2017',
+            description: 'I designed and developed a fully responsive website for the Alabama Cup Racing Association.',
+            images: ['images/acra-site.png']
+        }
+    ]
+};
+
 var bio = {
     name: 'Tod McChesney',
     role: 'Front-End Web Developer',
-    contact: {
+    contacts: {
         mobile: '+41 79 123 4567',
         email: 'tod@example.com',
         github: 'https://github.com/TodMcChesney',
         location: 'Geneva, Switzerland'
     },
-    message: 'I am passionate about front-end web development.',
+    welcomeMessage: 'I am passionate about front-end web development.',
     skills: ['HTML', 'CSS', 'JavaScript'],
-    picture: 'images/profile.jpg'
+    biopic: 'images/profile.jpg'
 };
-var formattedName = HTMLheaderName.replace('%data%', bio.name);
-var formattedRole = HTMLheaderRole.replace('%data%', bio.role);
-var formattedMobile = HTMLmobile.replace('%data%', bio.contact.mobile);
-var formattedEmail = HTMLemail.replace('%data%', bio.contact.email);
-var githubURL = HTMLgithub.replace('%data%', bio.contact.github);
-var formattedLocation = HTMLlocation.replace('%data%', bio.contact.location);
-var bioPic = HTMLbioPic.replace('%data%', bio.picture);
-var welcomeMsg = HTMLwelcomeMsg.replace('%data%', bio.message);
-var skillHTML = HTMLskills.replace('%data%', bio.skills[0]);
-var skillCSS = HTMLskills.replace('%data%', bio.skills[1]);
-var skillJavaScript = HTMLskills.replace('%data%', bio.skills[2]);
 
-$('#header').prepend(formattedRole);
-$('#header').prepend(formattedName);
-$('#topContacts').append(formattedMobile);
-$('#topContacts').append(formattedEmail);
-$('#topContacts').append(githubURL);
-$('#topContacts').append(formattedLocation);
-$('#header').append(bioPic);
-$('#header').append(welcomeMsg);
-$('#header').append(HTMLskillsStart);
-$('#header').append(skillHTML);
-$('#header').append(skillCSS);
-$('#header').append(skillJavaScript);
+var education = [
+    {
+        name: 'GED',
+        location: 'Birmingham, Alabama',
+        dates: 'Jan 1990'
+    }
+],
+[
+    {
+        title: 'Front-End Web Development',
+        school: 'Treehouse',
+        dates: 'Apr 2015',
+        url: 'https://teamtreehouse.com/todmcchesney'
+    }
+];
 
 
-$('#footerContacts').append(formattedMobile);
-$('#footerContacts').append(formattedEmail);
-$('#footerContacts').append(githubURL);
-$('#footerContacts').append(formattedLocation);
+// var formattedName = HTMLheaderName.replace('%data%', bio.name);
+// var formattedRole = HTMLheaderRole.replace('%data%', bio.role);
+// var formattedMobile = HTMLmobile.replace('%data%', bio.contact.mobile);
+// var formattedEmail = HTMLemail.replace('%data%', bio.contact.email);
+// var githubURL = HTMLgithub.replace('%data%', bio.contact.github);
+// var formattedLocation = HTMLlocation.replace('%data%', bio.contact.location);
+// var bioPic = HTMLbioPic.replace('%data%', bio.picture);
+// var welcomeMsg = HTMLwelcomeMsg.replace('%data%', bio.message);
+// var skillHTML = HTMLskills.replace('%data%', bio.skills[0]);
+// var skillCSS = HTMLskills.replace('%data%', bio.skills[1]);
+// var skillJavaScript = HTMLskills.replace('%data%', bio.skills[2]);
+//
+// $('#header').prepend(formattedRole);
+// $('#header').prepend(formattedName);
+// $('#topContacts').append(formattedMobile);
+// $('#topContacts').append(formattedEmail);
+// $('#topContacts').append(githubURL);
+// $('#topContacts').append(formattedLocation);
+// $('#header').append(bioPic);
+// $('#header').append(welcomeMsg);
+// $('#header').append(HTMLskillsStart);
+// $('#header').append(skillHTML);
+// $('#header').append(skillCSS);
+// $('#header').append(skillJavaScript);
+//
+//
+// $('#footerContacts').append(formattedMobile);
+// $('#footerContacts').append(formattedEmail);
+// $('#footerContacts').append(githubURL);
+// $('#footerContacts').append(formattedLocation);
